c1ec27636e1af48ee5a088d898b9a6fdfab61384
Yudai Nakajima <kituneudon97@gmail.com>
2024-03-16 18:14:17 +0900

    #3 Fast APIの動作確認のためだけに作成してエンドポイントを削除

--- api/source/main.py
+++ api/source/main.py
@@ -4,10 +4,6 @@ from source.schemas.reservation_form import ReservationFormCreate, ReservationFo
 
 app = FastAPI()
 
-@app.get("/")
-async def hello():
-  return {"message": "Hello World"}
-
 @app.post("/reservation-form", response_model=ReservationForm)
 async def create_reservation_form(reservation_form: ReservationFormCreate):
   return ReservationForm(id=1, **reservation_form.dict())
