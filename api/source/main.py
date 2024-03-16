16d08ba0cc8ef6f4b88a2df22803be7e091bc5b5
Nakajima <kituneudon97@gmail.com>
2024-03-16 18:15:21 +0900

    APIスキーマの作成 (#5)
    * #3 documentに最低限の情報が出るようになった
    
    * #3 ドキュメントで値例を表示するようにした
    
    * #3 postする側がidを指定するわけではないのでpost時のbodyからidを除外した
    
    * #3 Fast APIの動作確認のためだけに作成してエンドポイントを削除

--- api/source/main.py
+++ api/source/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+from source.schemas.reservation_form import ReservationFormCreate, ReservationForm
+
 app = FastAPI()
 
-@app.get("/")
-async def hello():
-  return {"message": "Hello World"}
+@app.post("/reservation-form", response_model=ReservationForm)
+async def create_reservation_form(reservation_form: ReservationFormCreate):
+  return ReservationForm(id=1, **reservation_form.dict())
