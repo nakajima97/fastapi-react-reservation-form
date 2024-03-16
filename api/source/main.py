4f8b17596a0baa11d38bfe96a2018fd9f8088c53
Yudai Nakajima <kituneudon97@gmail.com>
2024-03-16 18:13:29 +0900

    #3 postする側がidを指定するわけではないのでpost時のbodyからidを除外した

--- api/source/main.py
+++ api/source/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from source.schemas.reservation_form import ReservationForm
+from source.schemas.reservation_form import ReservationFormCreate, ReservationForm
 
 app = FastAPI()
 
@@ -8,6 +8,6 @@ app = FastAPI()
 async def hello():
   return {"message": "Hello World"}
 
-@app.post("/reservation-form")
-async def create_reservation_form(reservation_form: ReservationForm):
-  return reservation_form
+@app.post("/reservation-form", response_model=ReservationForm)
+async def create_reservation_form(reservation_form: ReservationFormCreate):
+  return ReservationForm(id=1, **reservation_form.dict())
--- api/source/schemas/reservation_form.py
+++ api/source/schemas/reservation_form.py
@@ -1,9 +1,14 @@
 import datetime
 from pydantic import BaseModel, Field
 
-class ReservationForm(BaseModel):
-    id: int
+class ReservationFromBase(BaseModel):
     reservation_date: datetime.date = Field(example="2024-01-01", description="予約日")
     name: str = Field(example="サンプル　太郎", max_length=255, description="名前")
     email_address: str = Field(example="example@example.com", max_length=255, description="メールアドレス")
     phone_number: str = Field(example="090-1234-5678", max_length=255, description="電話番号")
+
+class ReservationFormCreate(ReservationFromBase):
+    pass
+
+class ReservationForm(ReservationFromBase):
+    id: int
