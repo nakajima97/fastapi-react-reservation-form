82e89cf8539dc2d3bcaf02c9cd29251ff9a34c89
Yudai Nakajima <kituneudon97@gmail.com>
2024-03-16 18:08:38 +0900

    #3 ドキュメントで値例を表示するようにした

--- api/source/schemas/reservation_form.py
+++ api/source/schemas/reservation_form.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 
 class ReservationForm(BaseModel):
     id: int
-    reservation_date: datetime.date
-    name: str
-    email_address: str
-    phone_number: str
+    reservation_date: datetime.date = Field(example="2024-01-01", description="予約日")
+    name: str = Field(example="サンプル　太郎", max_length=255, description="名前")
+    email_address: str = Field(example="example@example.com", max_length=255, description="メールアドレス")
+    phone_number: str = Field(example="090-1234-5678", max_length=255, description="電話番号")
