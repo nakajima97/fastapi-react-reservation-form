8bfc791dbbadbb4979ebc1c33acc6aceaf098570
Nakajima <kituneudon97@gmail.com>
2024-03-20 09:32:42 +0900

    ReservationFormをReservationに修正 (#17)
    * #15 reserevation-formをreservationに修正した
    
    * #15 修正漏れ対応

--- api/source/schemas/reservation.py
+++ api/source/schemas/reservation.py
@@ -1,14 +1,14 @@
 import datetime
 from pydantic import BaseModel, Field
 
-class ReservationFromBase(BaseModel):
+class ReservationBase(BaseModel):
     reservation_date: datetime.date = Field(example="2024-01-01", description="予約日")
     name: str = Field(example="サンプル　太郎", max_length=255, description="名前")
     email_address: str = Field(example="example@example.com", max_length=255, description="メールアドレス")
     phone_number: str = Field(example="090-1234-5678", max_length=255, description="電話番号")
 
-class ReservationFormCreate(ReservationFromBase):
+class ReservationCreate(ReservationBase):
     pass
 
-class ReservationForm(ReservationFromBase):
+class Reservation(ReservationBase):
     id: int
