40cbd6f351e944be3e7b5633485c92771449d00f
nakajima97 <kituneudon97@gmail.com>
2024-03-21 09:21:43 +0900

    #8 値が保存されることを確認

--- api/source/main.py
+++ api/source/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 from source.schemas.reservation import ReservationCreate, Reservation
+from source.db import get_db
+from source.crud.reservation import create_reservation
 
 app = FastAPI()
 
 @app.post("/reservation", response_model=Reservation)
-async def create_reservation_form(reservation_form: ReservationCreate):
-  return Reservation(id=1, **reservation_form.dict())
+async def create_reservation_form(reservation_form: ReservationCreate, db: Session = Depends(get_db)):
+  return create_reservation(db, reservation_form)
--- api/source/models/reservations.py
+++ api/source/models/reservations.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, String, DateTime, Integer, Date
+from datetime import datetime
 
 from source.db import Base
 
 class Reservations(Base):
   __tablename__ = "reservations"
 
-  id = Column(Integer, primary_key=True, index=True)
+  id = Column(Integer, primary_key=True)
   reservation_date = Column(Date)
   name = Column(String(255))
   email_address = Column(String(255))
   phone_number = Column(String(255))
-  created_at = Column(DateTime)
+  created_at = Column(DateTime, default=datetime.now())
