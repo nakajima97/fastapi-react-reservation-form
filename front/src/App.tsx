c9dc13ce95248059f8db51056bbb220086732799
nakajima97 <kituneudon97@gmail.com>
2024-03-23 15:41:50 +0900

    #21 datepickerが表示されることを確認

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,27 @@
 import "./App.css";
-import Button from "@mui/material/Button";
+import { Box } from "@mui/system";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import "dayjs/locale/ja";
+import * as dayjs from "dayjs";
+import { Controller, useForm } from "react-hook-form";
+
+dayjs.locale("ja");
 
 function App() {
+  const { control } = useForm();
+
   return (
     <>
-      <Button variant="contained">Hello world</Button>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Box>
+          <Controller
+            name="date"
+            control={control}
+            render={({ field }) => <DatePicker {...field} />}
+          />
+        </Box>
+      </LocalizationProvider>
     </>
   );
 }
