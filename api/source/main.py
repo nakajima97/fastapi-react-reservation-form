41014b542f2055d00bde38fbc3254f18d1b255d1
Nakajima <kituneudon97@gmail.com>
2024-03-26 09:40:46 +0900

    #24 フォームから値を保存できることを確認 (#33)

--- api/source/main.py
+++ api/source/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from source.schemas.reservation import ReservationCreate, Reservation
@@ -6,6 +7,17 @@ from source.db import get_db
 from source.crud.reservation import create_reservation
 
 app = FastAPI()
+
+# origins = ["http://localhost:5173"]
+origins = ["*"]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 @app.post("/reservation", response_model=Reservation)
 async def create_reservation_form(reservation_form: ReservationCreate, db: Session = Depends(get_db)):
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,14 +6,34 @@ import "dayjs/locale/ja";
 import * as dayjs from "dayjs";
 import { Controller, useForm } from "react-hook-form";
 import { Button, Grid, TextField } from "@mui/material";
+import axios from "axios";
 
 dayjs.locale("ja");
+
+axios.defaults.withCredentials = true;
 
 function App() {
   const { register, control, handleSubmit } = useForm();
 
   const onSubmit = (data: any) => {
     console.log(data);
+    const postData = {
+      reservation_date: data.date.format("YYYY-MM-DD"),
+      name: data.name,
+      email_address: data.emailAddress,
+      phone_number: data.phoneNumber,
+    };
+    console.log({ postData });
+
+    const header = {
+      "Content-Type": "application/json",
+    };
+
+    axios
+      .post("http://localhost:8000/reservation", postData, header)
+      .then((res) => {
+        console.log(res);
+      });
   };
 
   return (
