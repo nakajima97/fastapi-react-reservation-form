0eac2038900ae6fdcd883164c0c8985465231a4c
nakajima97 <kituneudon97@gmail.com>
2024-03-28 08:12:42 +0900

    #32 corsエラーを解消

--- api/source/main.py
+++ api/source/main.py
@@ -8,10 +8,14 @@ from source.crud.reservation import create_reservation
 
 app = FastAPI()
 
+origins = [
+  'http://localhost:5173',
+]
+
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["*"],
-  allow_credentials=False,
+  allow_origins=origins,
+  allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
 )
