5124cf06c0ba93721b2ce9c3357979c2fc714789
Nakajima <kituneudon97@gmail.com>
2024-03-29 07:51:04 +0900

    空文字を防ぐために1文字以上のバリデーションを追加 (#49)

--- api/source/schemas/reservation.py
+++ api/source/schemas/reservation.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel, Field
 
 class ReservationBase(BaseModel):
     reservation_date: datetime.date = Field(example="2024-01-01", description="予約日")
-    name: str = Field(example="サンプル　太郎", max_length=255, description="名前")
-    email_address: str = Field(example="example@example.com", max_length=255, description="メールアドレス")
-    phone_number: str = Field(example="090-1234-5678", max_length=255, description="電話番号")
+    name: str = Field(example="サンプル　太郎", max_length=255, min_length=1, description="名前")
+    email_address: str = Field(example="example@example.com", max_length=255, min_length=1, description="メールアドレス")
+    phone_number: str = Field(example="090-1234-5678", max_length=255, min_length=1, description="電話番号")
 
 class ReservationCreate(ReservationBase):
     pass
