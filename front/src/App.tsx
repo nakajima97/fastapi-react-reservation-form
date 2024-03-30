c7729c2be7e8d7a5aba971f5c474b712b95b398d
Nakajima <kituneudon97@gmail.com>
2024-03-30 17:20:42 +0900

    使っていないimport boxを削除 (#69)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,3 @@
-import { Box } from "@mui/system";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import "dayjs/locale/ja";
