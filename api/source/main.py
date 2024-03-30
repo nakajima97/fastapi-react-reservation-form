171517776c35e6e1dc197e9c3b8dffe344518e92
Nakajima <kituneudon97@gmail.com>
2024-03-30 13:54:27 +0900

    休日を返すAPIのスキーマを開発 (#60)

--- api/source/main.py
+++ api/source/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from source.schemas.reservation import ReservationCreate, Reservation
+from source.schemas.holiday import Holidays
 from source.db import get_db
 from source.crud.reservation import create_reservation
 
@@ -23,3 +24,7 @@ app.add_middleware(
 @app.post("/reservation", response_model=Reservation)
 async def create_reservation_form(reservation_form: ReservationCreate, db: Session = Depends(get_db)):
   return create_reservation(db, reservation_form)
+
+@app.get("/holidays", response_model=Holidays)
+async def get_holidays():
+  return Holidays(holidays=["2024-01-01", "2024-01-02"])
