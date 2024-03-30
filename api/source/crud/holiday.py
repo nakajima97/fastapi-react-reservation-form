060919a57f6e56deb33df0536627b1c5067a1e2e
Yudai Nakajima <kituneudon97@gmail.com>
2024-03-30 15:39:40 +0900

    #59 休日である日程のみを取得するようにした

--- api/source/crud/holiday.py
+++ api/source/crud/holiday.py
@@ -4,5 +4,5 @@ from sqlalchemy.orm import Session
 import source.models.calendars as calendars_model
 
 def fetch_holidays(db: Session):
-    holidays = db.execute(select(calendars_model.Calendars.date)).fetchall()
+    holidays = db.execute(select(calendars_model.Calendars.date).where(calendars_model.Calendars.is_holiday == True)).fetchall()
     return {"holidays": [holiday[0] for holiday in holidays]}
